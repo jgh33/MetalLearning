34d08a68040305d7fd4aaf6230e60067544ee46c
jiaoguohui <1035390779@qq.com>
2021-07-12 20:34:49 +0800

    Initial Commit

---
+++ Shared/MetalLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalLearningApp.swift
+//  Shared
+//
+//  Created by 焦国辉 on 2021/7/12.
+//
+
+import SwiftUI
+
+@main
+struct MetalLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
